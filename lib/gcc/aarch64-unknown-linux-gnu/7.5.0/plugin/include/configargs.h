bf912988cc9ce734f63b0388fff34c323c6c6af9
bbtezcan <berkan.baris.tezcan@gmail.com>
2023-06-14 21:29:47 +0300

    Fixed libc and gcc version

---
+++ lib/gcc/aarch64-unknown-linux-gnu/7.5.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/bbtezcan/hive/rpi-toolchain/.build/aarch64-unknown-linux-gnu/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=aarch64-unknown-linux-gnu --prefix=/home/bbtezcan/x-tools/aarch64-unknown-linux-gnu --exec_prefix=/home/bbtezcan/x-tools/aarch64-unknown-linux-gnu --with-sysroot=/home/bbtezcan/x-tools/aarch64-unknown-linux-gnu/aarch64-unknown-linux-gnu/sysroot --enable-languages=c,c++ --with-pkgversion='crosstool-NG 1.25.0' --enable-__cxa_atexit --disable-tm-clone-registry --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --disable-libmpx --disable-libstdcxx-verbose --with-gmp=/home/bbtezcan/hive/rpi-toolchain/.build/aarch64-unknown-linux-gnu/buildtools --with-mpfr=/home/bbtezcan/hive/rpi-toolchain/.build/aarch64-unknown-linux-gnu/buildtools --with-mpc=/home/bbtezcan/hive/rpi-toolchain/.build/aarch64-unknown-linux-gnu/buildtools --with-isl=/home/bbtezcan/hive/rpi-toolchain/.build/aarch64-unknown-linux-gnu/buildtools --enable-lto --without-zstd --enable-threads=posix --enable-target-optspace --enable-plugin --enable-gold --disable-nls --disable-multilib --with-local-prefix=/home/bbtezcan/x-tools/aarch64-unknown-linux-gnu/aarch64-unknown-linux-gnu/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
